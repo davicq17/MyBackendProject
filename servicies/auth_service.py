c3ac9f4fa2d905731639874a75c202d9f8caab97
Estudiante IUB <estudiante@itsa.edu.local>
2026-06-10 20:04:09 -0500

    first commit

---
+++ servicies/auth_service.py
@@ -0,0 +1,26 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+
+from model.user_model import User
+from repositories.user_repository import UserRepository
+from schemas.user_schema import UserCreate
+from utils.security import hash_password
+
+class AuthService:
+    def __init__(self, db: Session):
+        self.user_repository = UserRepository(db=db)
+
+    def register(self, user: UserCreate):
+        existing_user = UserRepository(self.db).get_by_email(user.email)
+        if existing_user:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Email already registered"
+            )
+        new_user = User(
+            email= user.email,
+            full_name = user.full_name,
+            hashed_password = hash_password(user.password)
+        )
+
+        return self.user_repository.create(new_user)
