0d76b06d4829693b3783049fadf072ee6d28430e
davicq17 <davidcarrillo1043@gmail.com>
2026-06-15 16:36:19 -0500

    TaskRepository hecho falta probarlo y seguir con la capa de logica de negocios hasta finaliarlo

---
+++ model/task_model.py
@@ -0,0 +1,17 @@
+from datetime import datetime
+from sqlalchemy import String, DateTime, ForeignKey, func
+from sqlalchemy.orm import Mapped, mapped_column
+
+from config.database import Base
+
+class Task(Base):
+    __tablename__='tasks'
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[String] = mapped_column(String(100), nullable=False)
+    description: Mapped[str | None] = mapped_column(String(255), nullable= True)
+    status: Mapped[String]= mapped_column(String(50), default="pending")
+    priority: Mapped[String] = mapped_column(String(50), default = "medium")
+    owner_user: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now())
+    updated_at: Mapped[datetime] =   mapped_column(DateTime, server_default=func.now(), onupdate=func.now())
---
+++ schemas/task_schema.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+class TaskCreate(BaseModel):
+    title: str
+    description: str | None = None
+    priority: str
+
+class TaskResponse(BaseModel):
+    id: int
+    title: str
+    description: str | None
+    status: str
+    priority: str
+    owner_user: int 
+    created_at: datetime
+    update_at: datetime
+
+    model_config = {"from_attributes": True}
+
+class TaskUpdate(BaseModel):
+    title: str
+    description: str
+    status: str
+    priority: str
